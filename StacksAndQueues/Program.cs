c01d51349fcb17821d320e5186c92fe3dca34b89
Arka <arka.spectacular@gmail.com>
2021-08-07 14:32:59 +0530

    [Arka]Added:Enqueue Operation added

--- StacksAndQueues/Program.cs
+++ StacksAndQueues/Program.cs
@@ -6,17 +6,28 @@ namespace StacksAndQueues
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome To Stack And Queue Problem!");
-            Stack stack = new Stack();
-            stack.Push(70);
-            stack.Push(30);
-            stack.Push(56);
-            Console.WriteLine("Original stack is :");
-            stack.Display();
-            Console.WriteLine("\n"+"Stack after pop operation : ");
-            stack.Pop();
-            stack.Display();
-            stack.Peek();
+
+            Queue queue = new Queue();
+            queue.Enqueue(56);
+            queue.Enqueue(30);
+            queue.Enqueue(70);
+            queue.Display();
+
+
+
+
+
+            //Console.WriteLine("Welcome To Stack And Queue Problem!");
+            //Stack stack = new Stack();
+            //stack.Push(70);
+            //stack.Push(30);
+            //stack.Push(56);
+            //Console.WriteLine("Original stack is :");
+            //stack.Display();
+            //Console.WriteLine("\n"+"Stack after pop operation : ");
+            //stack.Pop();
+            //stack.Display();
+            //stack.Peek();
         }
     }
 }
