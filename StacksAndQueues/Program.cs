2a9e71d5cd1b0504b29e28edd4d12fea96e966ec
Arka <arka.spectacular@gmail.com>
2021-08-07 14:19:23 +0530

    [Arka]Added:Added Stack Push and Peek

---
+++ StacksAndQueues/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace StacksAndQueues
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Stack And Queue Problem!");
+            Stack stack = new Stack();
+            stack.Push(70);
+            stack.Push(30);
+            stack.Push(56);
+            stack.Display();
+            stack.Peek();
+        }
+    }
+}
